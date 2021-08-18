5019b58bd17b5fc0e6e932e56f6f44cb5dfdc7d6
Alea Vikraman <AleaMadeAWebsite@gmail.com>
2021-08-17 22:18:19 -0400

    set up api call to github

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import React, { useState, useEffect } from "react";
 import "./App.scss";
+import axios from "axios";
 
 function App() {
+  const [repo, setRepoData] = useState([]);
+
+  const gitHubUrl =
+    "https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&page=1&per_page=100";
+
+  const getApiData = async () => {
+    try {
+      const response = await axios.get(gitHubUrl);
+      setRepoData(response.data);
+      console.log("response", response.data.items);
+    } catch (error) {
+      console.log("api error", error);
+    }
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, []);
   return <div className="App"></div>;
 }
 
